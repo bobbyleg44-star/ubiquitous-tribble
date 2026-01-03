3a6e361f636929ee4b78cd9df4c6ff48810114e5
bobbyleg44-star <bobbyleg44@gmail.com>
2026-01-03 13:50:07 +0100

    Fix typo in event listener method name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Find the button and paragraph by their IDs
 const button = document.getElementById("myButton");
 const demo = document.getElementById("demo");
-button.addEventListener("click", () => {
+button.add EventListener("click", () => {
   demo.textContent = "Nice! You’re learning JavaScript 🚀";
   demo.style.color = "#3333cc";
 });
