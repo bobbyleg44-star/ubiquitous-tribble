b94918629a2829e79f6dd6bbdd360c1c57185fb9
bobbyleg44-star <bobbyleg44@gmail.com>
2026-01-03 11:52:16 +0100

    Add greeting functionality to button click

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ button.addEventListener("click", () => {
   demo.textContent = "Nice! You’re learning JavaScript 🚀";
   demo.style.color = "#3333cc";
 });
+const greetButton = document.getElementById("greetButton");
+const nameInput = document.getElementById("nameInput");
+const greeting = document.getElementById("greeting");
+
+greetButton.addEventListener("click", () => {
+  const name = nameInput.value;
+
+  if (name === "") {
+    greeting.textContent = "Please enter your name 🙂";
+  } else {
+    greeting.textContent = "Hello, " + name + "! Welcome to my website 🚀";
+  }
+});
 
